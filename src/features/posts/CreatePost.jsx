46470871c72a02e6f8d52fb976e2ce9dba35d1d9
kaushalendra pateriya <kaushalendrapateriya@kaushalendras-MacBook-Air.local>
2026-01-08 14:28:32 +0530

    change to check

--- src/features/posts/CreatePost.jsx
+++ src/features/posts/CreatePost.jsx
@@ -80,7 +80,7 @@ export default function CreatePost() {
           )}
           <Box sx={{ flex: 1 }}>
             <TextField
-              placeholder="Create Post"
+              placeholder="Create Posts"
               fullWidth
               value={title}
               onChange={(e) => setTitle(e.target.value)}
